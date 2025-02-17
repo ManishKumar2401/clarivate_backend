2a560f665fa45e29d44c1ee908ee550159d97e4b
Manish Kumar <manishk2401@gmail.com>
2025-02-17 16:36:26 +0530

    db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './products/products.module';
 import { CategoryModule } from './categories/categories.module';
-import { Product } from './products/entities/product.entity';
-import { Category } from './categories/entities/category.entity';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '9211',
-      database: "postgres",
-      entities: [Product, Category],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     ProductModule,
     CategoryModule
   ],
